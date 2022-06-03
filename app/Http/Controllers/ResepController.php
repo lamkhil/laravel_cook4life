43e7c3f7f5d7fe0f7083b4306947d468de684b43
lamkhil <43593728+lamkhil@users.noreply.github.com>
2022-06-03 23:07:02 +0700

    try fix rekomendasi 3

--- app/Http/Controllers/ResepController.php
+++ app/Http/Controllers/ResepController.php
@@ -43,7 +43,7 @@ class ResepController extends Controller
                 request(['search', 'kategori_id', 'kategori']))
             ->with(['kategori', 'user', 'bahan', 'langkah'])
             ->withCount(['like', 'favorit', 'like_me','favorit_me'])
-            ->paginate(5))->sortBy('like');
+            ->paginate(5));
     }
 
     /**
