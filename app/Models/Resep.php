bf71b2c5a4263fa6a34324ec16ada619df47ab84
lamkhil <43593728+lamkhil@users.noreply.github.com>
2022-06-05 17:55:12 +0700

    fix count me

--- app/Models/Resep.php
+++ app/Models/Resep.php
@@ -4,75 +4,95 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Resep extends Model
 {
     public $table = "resep";
     use HasFactory;
 
-    protected $guarded =[];
+    protected $guarded = [];
 
-    public function kategori(){
+    public function kategori()
+    {
         return $this->belongsTo(Kategori::class, 'kategori_id');
     }
 
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function bahan(){
+    public function bahan()
+    {
         return $this->hasMany(Bahan::class)->with('toko');
     }
 
-    public function langkah(){
+    public function langkah()
+    {
         return $this->hasMany(Langkah::class);
     }
 
-    public function like(){
+    public function like()
+    {
         return $this->hasMany(Like::class);
     }
 
-    public function komentar(){
+    public function komentar()
+    {
         return $this->hasMany(Komentar::class);
     }
 
-    public function like_me(){
-        $request = request();
-        return $this->hasMany(Like::class)->whereUserId($request->user()->id);
+    public function like_me()
+    {
+        if (Auth::check()) {
+            return $this->hasMany(Like::class)->whereUserId(Auth::id());
+        } else {
+            return [];
+        }
     }
 
-    public function favorit_me(){
-        $request = request();
-        return $this->hasMany(Favorit::class)->whereUserId($request->user()->id);
+    public function favorit_me()
+    {
+        if (Auth::check()) {
+            return $this->hasMany(Favorit::class)->whereUserId(Auth::id());
+        } else {
+            return [];
+        }
     }
 
-    public function rating(){
-        $request = request();
-        return $this->hasMany(Rating::class)->whereUserId($request->user()->id);
+    public function rating()
+    {
+        if (Auth::check()) {
+            return $this->hasMany(Rating::class)->whereUserId(Auth::id());
+        } else {
+            return [];
+        }
     }
 
-    public function favorit(){
+    public function favorit()
+    {
         return $this->hasMany(Favorit::class);
     }
 
-    public function scopeFilter($query, array $filters){
-        
-        $query->when($filters['search']??false, function($query, $search){
-            return $query->where('nama_resep', 'like', '%'.$search.'%')
-                        ->orWhere('deskripsi', 'like', '%'.$search.'%')
-                        ->orWhereHas('kategori', function($query) use ($search){
-                            $query->where('nama_kategori', 'like', '%'.$search.'%');
-                        });
+    public function scopeFilter($query, array $filters)
+    {
+
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            return $query->where('nama_resep', 'like', '%' . $search . '%')
+                ->orWhere('deskripsi', 'like', '%' . $search . '%')
+                ->orWhereHas('kategori', function ($query) use ($search) {
+                    $query->where('nama_kategori', 'like', '%' . $search . '%');
+                });
         });
 
-        $query->when($filters['kategori_id']??false, function($query, $kategori_id){
+        $query->when($filters['kategori_id'] ?? false, function ($query, $kategori_id) {
             return $query->where('kategori_id', $kategori_id);
         });
 
-        $query->when($filters['kategori']??false, function($query, $kategori){
-            return $query->whereHas('kategori', function($query) use ($kategori){
-                $query->where('nama_kategori', 'like', '%'.$kategori.'%');
+        $query->when($filters['kategori'] ?? false, function ($query, $kategori) {
+            return $query->whereHas('kategori', function ($query) use ($kategori) {
+                $query->where('nama_kategori', 'like', '%' . $kategori . '%');
             });
         });
     }
