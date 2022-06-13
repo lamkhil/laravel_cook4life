760a8af01e6f862c57e59e20f5d4ff4ee1bf2861
lamkhil <43593728+lamkhil@users.noreply.github.com>
2022-06-14 00:49:22 +0700

    cahnge api method update

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,7 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::get('/resep', [ResepController::class, 'index']);
     Route::get('/resep/{id}', [ResepController::class, 'show']);
     Route::post('/resep', [ResepController::class, 'store']);
-    Route::patch('/resep/{resep}', [ResepController::class, 'update']);
+    Route::post('/resep/{resep}', [ResepController::class, 'update']);
     Route::delete('/resep/{resep}', [ResepController::class, 'destroy']);
 
     Route::post('/like', [ResepController::class, 'like']);
